6f8dba48a693e0e81c31f298612ff131aab08a92
KeiShiba-1218 <keishiba1218@gmail.com>
2023-03-21 11:27:45 +0000

    cycle consistencyの実装

--- train_codes/train_cycle.py
+++ train_codes/train_cycle.py
@@ -5,6 +5,7 @@ import json
 import os
 import shutil
 import time
+import itertools
 
 import torch
 import torch.utils
@@ -15,7 +16,7 @@ from PIL import Image
 
 from datasets.dataset import UnpairedImageDataset
 from scripts.losses import GANLoss, cal_gradient_penalty
-from scripts.utils import load_option, pad_tensor, send_line_notify, tensor2ndarray, arrange_images
+from scripts.utils import load_option, pad_tensor, send_line_notify, tensor2ndarray, arrange_images, set_requires_grad
 from scripts.cal_fid import get_fid
 from scripts.optimizer import CosineLRWarmup
 
@@ -37,10 +38,11 @@ def train(opt_path):
     os.makedirs(log_dir, exist_ok=True)
 
     log_items_train = [
-        'total_step','lr_G','lr_D', 'loss_D_real', 'loss_D_fake', 'gp', 'loss_D', 'loss_G_fake', 'recons_loss', 'loss_G'
+        'total_step', 'lr', 'loss_G_GA', 'loss_G_GB', 'loss_G_cycle_A', 'loss_G_cycle_B', 
+        'loss_D_B', 'loss_D_GA', 'loss_D_AB', 'loss_D_BA_A', 'loss_D_BA_GB', 'loss_D_BA'
     ]
     log_items_val = [
-        'total_step', 'fid_score'
+        'total_step', 'fid_score_A', 'fid_score_B'
     ]
 
     with open(log_path, mode='w', encoding='utf-8') as fp: fp.write('')
@@ -51,21 +53,21 @@ def train(opt_path):
     
     shutil.copy(opt_path, f'./experiments/{opt.name}/{os.path.basename(opt_path)}')
     
-    loss_fn = GANLoss(gan_mode='wgangp').to(device)
+    loss_fn = GANLoss(gan_mode='lsgan').to(device)
     network_module_G = importlib.import_module(opt.network_module_G)
-    netAB = getattr(network_module_G, opt.model_type_G)(opt.netAB).to(device)
+    netG = getattr(network_module_G, opt.model_type_G)(**opt.netG).to(device)
+    network_module_G_BA = importlib.import_module(opt.network_module_G_BA)
+    netG_BA = getattr(network_module_G_BA, opt.model_type_G_BA)(**opt.netG_BA).to(device)
     network_module_D = importlib.import_module(opt.network_module_D)
     netD = getattr(network_module_D, opt.model_type_D)(opt.netD).to(device)
+    network_module_D_BA = importlib.import_module(opt.network_module_D_BA)
+    netD_BA = getattr(network_module_D_BA, opt.model_type_D_BA)(opt.netD_BA).to(device)
 
-    optimG = torch.optim.Adam(netAB.parameters(), lr=opt.learning_rate_G, betas=opt.betas)
+    optimG = torch.optim.Adam(itertools.chain(netG.parameters(), netG_BA.parameters()), lr=opt.learning_rate, betas=opt.betas)
     schedulerG = CosineLRWarmup(optimG, opt.lr_w, opt.lr_max, opt.lr_min, opt.step_w, opt.step_max)
-    optimD = torch.optim.Adam(netD.parameters(), lr=opt.learning_rate_D, betas=opt.betas)
+    optimD = torch.optim.Adam(itertools.chain(netD.parameters(), netD_BA.parameters()), lr=opt.learning_rate, betas=opt.betas)
     schedulerD = CosineLRWarmup(optimD, opt.lr_w, opt.lr_max, opt.lr_min, opt.step_w, opt.step_max)
 
-    if opt.pretrained_path:
-        state_dict = torch.load(opt.pretrained_path, map_location=device)
-        netAB.load_state_dict(state_dict['netAB_state_dict'], strict=True)
-        optimG.load_state_dict(state_dict['optimG_state_dict'])
 
     train_dataset = UnpairedImageDataset(opt.trainA_path, opt.trainB_path, opt.input_resolution, opt.data_extention, opt.cache_images)
     train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=opt.batch_size, shuffle=True, num_workers=2)
@@ -76,47 +78,60 @@ def train(opt_path):
     start_time = time.time()
     total_step = 0 if opt.resume_step is None else opt.resume_step
     best_fid = float('inf')
-    netAB.train()
+    netG.train()
     for e in range(1, 42*opt.steps):
         for i, data in enumerate(train_loader):
             # [(B,C,H,W)]*F -> (B,F,C,H,W)
             A = data['A'].to(device)
             B = data['B'].to(device)
-
-            # Training D
-            netD.zero_grad()
-            logits_real = netD(B)
-            loss_D_real = opt.coef_adv*loss_fn(logits_real, target_is_real=True)
-
-            fake = netAB(A)
-            logits_fake = netD(fake.detach())
-            loss_D_fake = opt.coef_adv*loss_fn(logits_fake, target_is_real=False)
-
-            gp = opt.coef_gp*cal_gradient_penalty(netD, B, fake, device)[0]
-
-            loss_D = loss_D_real + loss_D_fake + gp
-            loss_D.backward(retain_graph=True)
-            if opt.use_grad_clip: torch.nn.utils.clip_grad_norm_(netD.parameters(), opt.grad_clip_val)
-            optimD.step()
-            schedulerD.step()
+            GA = netG(A)
+            GB = netG_BA(B)
+            A_rec = netG_BA(GA)
+            B_rec = netG(GB)
 
             # Training G
-            netAB.zero_grad()
-            logits_fake = netD(fake)
-            loss_G_fake = opt.coef_adv*loss_fn(logits_fake, target_is_real=True)
-            loss_recons = opt.coef_l1*F.mse_loss(A, fake)
-            
-            loss_G = loss_G_fake + loss_recons
+            set_requires_grad([netD, netD_BA], False)
+            netG.zero_grad()
+            logits_GA = netD(GA)
+            loss_G_GA = opt.coef_adv*loss_fn(logits_GA, target_is_real=True)
+            logits_GB = netD_BA(GB)
+            loss_G_GB = opt.coef_adv*loss_fn(logits_GB, target_is_real=True)
+            loss_G_cycle_A = opt.coef_cycle*F.l1_loss(A, A_rec)
+            loss_G_cycle_B = opt.coef_cycle*F.l1_loss(B, B_rec)
+
+            loss_G = loss_G_GA + loss_G_GB + loss_G_cycle_A + loss_G_cycle_B
             loss_G.backward()
-            if opt.use_grad_clip: torch.nn.utils.clip_grad_norm_(netAB.parameters(), opt.grad_clip_val)
+            if opt.use_grad_clip: torch.nn.utils.clip_grad_norm_(netG.parameters(), opt.grad_clip_val)
+            if opt.use_grad_clip: torch.nn.utils.clip_grad_norm_(netG_BA.parameters(), opt.grad_clip_val)
             optimG.step()
             schedulerG.step()
+
+            # Training D
+            set_requires_grad([netD, netD_BA], True)
+            netD.zero_grad()
+            logits_B = netD(B)
+            loss_D_B = opt.coef_adv*loss_fn(logits_B, target_is_real=True)
+            logits_GA = netD(GA.detach())
+            loss_D_GA = opt.coef_adv*loss_fn(logits_GA, target_is_real=False)
+            loss_D_AB = loss_D_B + loss_D_GA
+            loss_D_AB.backward()
+
+            logits_D_BA_A = netD(A)
+            loss_D_BA_A = opt.coef_adv*loss_fn(logits_D_BA_A, target_is_real=True)
+            logits_D_BA_GB = netD(GB.detach())
+            loss_D_BA_GB = opt.coef_adv*loss_fn(logits_D_BA_GB, target_is_real=False)
+            loss_D_BA = loss_D_BA_A + loss_D_BA_GB
+            loss_D_BA.backward()
+
+            if opt.use_grad_clip: torch.nn.utils.clip_grad_norm_(netD.parameters(), opt.grad_clip_val)
+            if opt.use_grad_clip: torch.nn.utils.clip_grad_norm_(netD_BA.parameters(), opt.grad_clip_val)
+            optimD.step()
+            schedulerD.step()
             
             total_step += 1
 
             if total_step%1==0:
-                lr_G = [group['lr'] for group in optimG.param_groups][0]
-                lr_D = [group['lr'] for group in optimD.param_groups][0]
+                lr = [group['lr'] for group in optimG.param_groups][0]
                 lg = ''
                 for logname_idx, name in enumerate(log_items_train):
                     val = eval(name)
@@ -145,7 +160,8 @@ def train(opt_path):
 
             if total_step%opt.eval_freq==0:
                 # Validation
-                netAB.eval()
+                netG.eval()
+                netG_BA.eval()
                 for i, data in enumerate(val_loader):
                     # [(B,C,H,W)]*F -> (B,F,C,H,W)
                     A = data['A'].to(device)
@@ -153,30 +169,35 @@ def train(opt_path):
                     b,c,h,w = A.shape
                     A, B = map(lambda x: pad_tensor(x, divisible_by=2**3), [A, B])
                     with torch.no_grad():
-                        fake = netAB(A)
+                        GA = netG(A)
+                        GB = netG_BA(B)
                     
-                    A, B, fake = map(lambda x: tensor2ndarray(x)[0,:h,:w,:], [A, B, fake])
+                    A, B, GA, GB = map(lambda x: tensor2ndarray(x)[0,:h,:w,:], [A, B, GA, GB])
 
                     out_dir_A = os.path.join(image_out_dir, f'{str(total_step).zfill(len(str(opt.steps)))}', 'A')
                     out_dir_B = os.path.join(image_out_dir, f'{str(total_step).zfill(len(str(opt.steps)))}', 'B')
-                    out_dir_fake = os.path.join(image_out_dir, f'{str(total_step).zfill(len(str(opt.steps)))}', 'fake')
+                    out_dir_GA = os.path.join(image_out_dir, f'{str(total_step).zfill(len(str(opt.steps)))}', 'GA')
+                    out_dir_GB = os.path.join(image_out_dir, f'{str(total_step).zfill(len(str(opt.steps)))}', 'GB')
 
                     os.makedirs(out_dir_A, exist_ok=True)
                     os.makedirs(out_dir_B, exist_ok=True)
-                    os.makedirs(out_dir_fake, exist_ok=True)
-                    A, B, fake = map(lambda x: Image.fromarray(x), [A, B, fake])
+                    os.makedirs(out_dir_GA, exist_ok=True)
+                    os.makedirs(out_dir_GB, exist_ok=True)
+                    A, B, GA, GB = map(lambda x: Image.fromarray(x), [A, B, GA, GB])
                     A.save(os.path.join(out_dir_A, f'{i:04}.{opt.save_extention}'))
                     B.save(os.path.join(out_dir_B, f'{i:04}.{opt.save_extention}'))
-                    fake.save(os.path.join(out_dir_fake, f'{i:04}.{opt.save_extention}'))
+                    GA.save(os.path.join(out_dir_GA, f'{i:04}.{opt.save_extention}'))
+                    GB.save(os.path.join(out_dir_GB, f'{i:04}.{opt.save_extention}'))
                     if opt.save_compare:
                         out_dir_compare = os.path.join(image_out_dir, f'{str(total_step).zfill(len(str(opt.steps)))}', 'compare')
                         os.makedirs(out_dir_compare, exist_ok=True)
-                        arrange_images([A, fake, B]).save(os.path.join(out_dir_compare, f'{i:04}.{opt.save_extention}'))
-                netAB.train()
+                        arrange_images([A, GA, B, GB]).save(os.path.join(out_dir_compare, f'{i:04}.{opt.save_extention}'))
+                netG.train()
                 
-                fid_score = get_fid([out_dir_fake, out_dir_B], batch_size=32, dims=2048, num_workers=2) 
+                fid_score_A = get_fid([out_dir_GB, out_dir_A], batch_size=32, dims=2048, num_workers=2) 
+                fid_score_B = get_fid([out_dir_GA, out_dir_B], batch_size=32, dims=2048, num_workers=2) 
                     
-                txt = f'FID: {fid_score:f}'
+                txt = f'FID: {fid_score_A:f}, {fid_score_B:f}'
                 # print(txt)
                 with open(log_path, mode='a', encoding='utf-8') as fp:
                     fp.write(txt+'\n')
@@ -190,11 +211,11 @@ def train(opt_path):
                 with open(log_test_losses_paths, mode='a', encoding='utf-8') as fp:
                     fp.write(lg+'\n')
                 
-                if fid_score <= best_fid:
-                    best_fid = fid_score
+                if fid_score_B <= best_fid:
+                    best_fid = fid_score_B
                     torch.save({
                         'total_step': total_step,
-                        'netAB_state_dict': netAB.state_dict(),
+                        'netG_state_dict': netG.state_dict(),
                         'optimG_state_dict': optimG.state_dict(),
                         'schedularG_state_dict': schedulerG.state_dict(),
                         'netD_state_dict': netD.state_dict(),
@@ -210,7 +231,7 @@ def train(opt_path):
             if total_step%opt.save_freq==0:
                 torch.save({
                     'total_step': total_step,
-                    'netAB_state_dict': netAB.state_dict(),
+                    'netG_state_dict': netG.state_dict(),
                     'optimG_state_dict': optimG.state_dict(),
                     'schedularG_state_dict': schedulerG.state_dict(),
                     'netD_state_dict': netD.state_dict(),
@@ -221,7 +242,7 @@ def train(opt_path):
             if total_step==opt.steps:
                 torch.save({
                     'total_step': total_step,
-                    'netAB_state_dict': netAB.state_dict(),
+                    'netG_state_dict': netG.state_dict(),
                     'optimG_state_dict': optimG.state_dict(),
                     'schedularG_state_dict': schedulerG.state_dict(),
                     'netD_state_dict': netD.state_dict(),
